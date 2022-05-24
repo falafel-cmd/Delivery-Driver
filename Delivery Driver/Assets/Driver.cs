3c46a9af53bda4b96509c665fe0700991492133f
Fathi Mokhtar <70948032+falafel-cmd@users.noreply.github.com>
2022-05-24 22:12:07 +0100

    Complete implementation of boost and bump
    player couldn't move due to no assignments for the moveSpeed initially
    
    initial implementation of boosts & slows
    
    bug fix: objects didnt act as proper trigger
    
    toggles isTrigger to True for applicable gameObjects tagged with "Bump" & "Boost"
    
    final fixes for bumps & boosts

--- Delivery Driver/Assets/Driver.cs
+++ Delivery Driver/Assets/Driver.cs
@@ -8,24 +8,50 @@ public class Driver : MonoBehaviour
 {
 
     #region Variables
-    [SerializeField] float steerSpeed = 0.2f;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float steerSpeed = 150f;
+    [SerializeField] float baseSpeed = 6.5f;
+    [SerializeField] float slowSpeed = 5f;
+    [SerializeField] float boostSpeed = 8f;
+
+    float moveSpeed = 0f;
+
     #endregion
 
     #region Unity Methods
 
     void Start()
-    {
-
-    }
-
-
+	{
+        moveSpeed = baseSpeed;
+	}
     void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+
+    IEnumerator Waiter()
+    {
+        yield return new WaitForSecondsRealtime(5f);
+        //Car gets slowed for a certain amount of irl seconds
+        moveSpeed = baseSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+	{
+        if (other.tag == "Boost")
+		{
+            moveSpeed = boostSpeed;
+            Debug.Log("Your car is speeding up!");
+            StartCoroutine("Waiter");
+		}
+        if (other.tag == "Bump")
+        {
+            moveSpeed = slowSpeed;
+            Debug.Log("Oh no! The car's be slowed down");
+            StartCoroutine("Waiter");
+        }
     }
 
     #endregion
